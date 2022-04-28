ba9c55f59f5c08cdd9a92f68eb09ca30d48e7886
anton <a.sypavka@1plus1.digital>
2022-04-28 14:49:36 +0300

    initial commit

--- configs/config.js
+++ configs/config.js
@@ -8,6 +8,4 @@ module.exports = {
     AWS_S3_NAME: process.env.AWS_S3_NAME,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_PRIVAT_KEY,
-
-    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://localhost:3000;http://localhost:8080'
 };
