d0042c9bbfdd1561e0bcb19c1ab83e3d928daada
anton <a.sypavka@1plus1.digital>
2022-04-28 20:18:52 +0300

    fix: hero validation

--- validators/superhero.validator.js
+++ validators/superhero.validator.js
@@ -3,31 +3,27 @@ const Joi = require('joi');
 const createSuperheroValidator = new Joi.object({
     nickname: Joi
         .string()
-        .min(3)
         .trim()
         .required(),
 
     real_name: Joi
         .string()
-        .min(3)
         .trim()
         .required(),
 
     origin_description: Joi
         .string()
-        .min(15)
         .trim()
+        .min(10)
         .required(),
 
     superpowers: Joi
         .string()
-        .min(10)
         .trim()
         .required(),
 
     catch_phrase: Joi
         .string()
-        .min(5)
         .trim()
         .required(),
 });
