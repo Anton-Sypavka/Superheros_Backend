bcf4f730a9ebf839f478310caab3bbc159943cc4
anton <a.sypavka@1plus1.digital>
2022-04-28 18:58:10 +0300

    initial commit

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const expressFileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
-// const morgan = require('morgan');
-
-const formidableMiddleware = require('express-formidable');
+const morgan = require('morgan');
 
 const { PORT, DATABASE, ALLOWED_ORIGIN } = require('./configs/config');
 const { superherosRouter } = require('./routes');
@@ -12,7 +10,6 @@ const { superherosRouter } = require('./routes');
 mongoose.connect(DATABASE);
 const app = express();
 
-// app.use(formidableMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(expressFileUpload());
@@ -20,9 +17,9 @@ app.use(cors());
 app.options('*', cors());
 
 
-// if (process.env.ENV === 'development') {
-//     app.use(morgan('dev'));
-// }
+if (process.env.ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use('/superheros', superherosRouter);
 app.get('/ping', (req, res) => res.send('pong'));
@@ -40,18 +37,4 @@ function _globalErrorHandler(err, req, res, next) {
         .json({
             message: err.message || 'Something went wrong'
         });
-}
-
-function _configureCors(origin, callback) {
-    const whiteList = ALLOWED_ORIGIN.split(';');
-
-    if (!origin) {
-        return callback(null, true);
-    }
-
-    if (!whiteList.includes(origin)) {
-        return callback(new Error('Cors not allowed'), false);
-    }
-
-    return callback(null, true);
 }
